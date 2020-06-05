1e40a3bd0eee45844f2dbbfbfb2ba0074ad0b2af
Hayri Yigit <hayriyigit@protonmail.com>
2020-06-05 23:44:49 +0300

    Set threads

--- multi_matrix.c
+++ multi_matrix.c
@@ -4,9 +4,12 @@
 
 void *matrix_multiplication_runner(void *t)
 {
-    long a = (long)t;
-    printf("I'm thread %d", a);
+    int a = *((int *)t);
+
+    printf("Thread %d started \n", a);
     pthread_exit(0);
+
+
 }
 
 int main(int argc, char *argv[])
@@ -15,23 +18,26 @@ int main(int argc, char *argv[])
     if (argc >= 2)
     {
         threads = atoi(argv[1]);
+        printf("Number of threads: %d\n",threads);
     }
     else
     {
         //we assume that the user is at least running on a dual core
         threads = 2;
-    }
-    pthread_t tids[threads];
-    for (int i = 0; i < threads; i++)
-    {
-        pthread_attr_t attr;
-        pthread_attr_init(&attr);
-        pthread_create(&tids[i], &attr, matrix_multiplication_runner, &i);
-    }
-
-    for (int i = 0; i < threads; i++)
-    {
-        pthread_join(tids[i], NULL);
         
     }
+
+    pthread_t tids[threads];
+    pthread_attr_t attr;
+    
+    for (int i = 0; i < threads; i++)
+    {
+        pthread_attr_init(&attr);
+        pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+        pthread_create(&tids[i], &attr, matrix_multiplication_runner, &i);
+        pthread_join(tids[i], NULL);
+    }
+
+
+
 }
