2eb628801ae35e068c3fa5606f3d4d6462cdc900
Francisco Molina <molinajimenez01@gmail.com>
2020-06-07 16:13:59 +0200

    idk what changed but ok

--- utils.h
+++ utils.h
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-struct MatrixBase{
-    int row1,col1;
-    int row2,col2;
-    int min,max;
+struct MatrixBase
+{
+    int row1, col1;
+    int row2, col2;
+    int min, max;
     float **matrix1;
     float **matrix2;
 };
 
-float GetRandomFloatNumber(float min, float max)    
-{  
-    return (min + 1) + (((float) rand()) / (float) RAND_MAX) * (max - (min + 1));    
+float GetRandomFloatNumber(float min, float max)
+{
+    return (min + 1) + (((float)rand()) / (float)RAND_MAX) * (max - (min + 1));
 }
