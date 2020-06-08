f25e464f2ef9f80c2b71c37ea26af67b5de141f3
Francisco Molina <molinajimenez01@gmail.com>
2020-06-08 21:28:29 +0200

    deleted hardcoding and old code

--- multi_matrix.c
+++ multi_matrix.c
@@ -10,50 +10,35 @@ void *matrix_multiplication_runner(void *arg)
 {
     double localSum;
     struct MatrixBase *local_struct = (struct MatrixBase *)arg;
-    // printf("Col1 = %d\n",global_struct.col1);
-    // pthread_mutex_lock(&mutex);
-    printf("Thread: %d\n",local_struct->num_threads);
-
+    
     for (int i = 0; i < local_struct->row1; i++)
     {
         for (int j = 0; j < local_struct->col2; j++)
         {
             localSum = 0;
-            for (int k = local_struct->num_threads; k < local_struct->row2; k += 4)
+            for (int k = local_struct->num_threads; k < local_struct->row2; k += global_struct.leap)
             {
                 localSum = localSum + (local_struct->matrix1[i][k] * local_struct->matrix2[k][j]);
             }
             local_struct->resultMatrix[i][j] = localSum;
 
-            // printf("LOCAL SUM = %lf\n",localSum);
+            
         }
     }
-    // pthread_mutex_unlock(&mutex);
 
     pthread_exit(0);
 }
 
 int main(int argc, char *argv[])
 {
-    // if (argc >= 2)
-    // {
-
-    //     //params.threads = atoi(argv[1]);
-    //     printf("OKI");
-    // }
-    // else
-    // {
-    //     //we assume that the user is at least running on a dual core processor
-
-    //     printf("OKI");
-    // }
     global_struct = GenerateMatrices(argv);
     global_struct = FillMatrices(global_struct);
-
-    pthread_t tids[4];
+    int threads = atoi(argv[6]);
+    global_struct.leap = threads;
+    pthread_t tids[global_struct.leap];
     pthread_attr_t attr;
     // pthread_mutex_init(&mutex, NULL);
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < global_struct.leap; i++)
     {
         global_struct.num_threads = i;
         pthread_attr_init(&attr);
@@ -61,7 +46,7 @@ int main(int argc, char *argv[])
         pthread_create(&tids[i], &attr, matrix_multiplication_runner, &global_struct);
     }
 
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < global_struct.leap; i++)
     {
         pthread_join(tids[i], NULL);
     }
